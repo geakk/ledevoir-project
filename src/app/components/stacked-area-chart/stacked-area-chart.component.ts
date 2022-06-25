92e3690312690cd67baa61a2d27f124a544e5433
jamesley55 <Jamesleyjoseph5@gmail.com>
2022-06-25 13:00:45 -0700

    starting of stacked-area-chart

---
+++ src/app/components/stacked-area-chart/stacked-area-chart.component.ts
@@ -0,0 +1,81 @@
+import { AfterViewInit, Component, ElementRef, Input } from '@angular/core';
+import * as d3 from 'd3';
+import { Covid, DataEntry } from '../../interfaces/data-entry.interface';
+import { DataService } from '../../services/data.service';
+import { FilterEventsService } from '../../services/filter-events.service';
+import { ThemeService } from '../../services/theme.service';
+
+@Component({
+  selector: 'app-stacked-area-chart',
+  templateUrl: './stacked-area-chart.component.html',
+  styleUrls: ['./stacked-area-chart.component.scss'],
+})
+export class StackedAreaChartComponent implements AfterViewInit {
+  @Input() articlesData: DataEntry[] = [];
+  @Input() covidData: Covid[] = [];
+  public data: any;
+
+  private width = 800;
+  private height = 700;
+  private margin = 50;
+
+  public yScale: d3.AxisScale<number> | undefined;
+  public xScale: d3.AxisScale<Date> | undefined;
+
+  public svg: d3.Selection<SVGSVGElement, unknown, null, undefined> | undefined;
+
+  constructor(
+    // eslint-disable-next-line no-unused-vars
+    public chartElem: ElementRef,
+    private readonly host: ElementRef,
+    // eslint-disable-next-line no-unused-vars
+    private readonly theme: ThemeService,
+    // eslint-disable-next-line no-unused-vars
+    private readonly filter: FilterEventsService,
+    private dataService: DataService
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.data = this.dataService.getNbArticlesByDay(this.articlesData);
+    //this.createChart();
+  }
+
+  createChart() {
+    let casCovid = this.covidData.map((d) => d['Cas confirmés']);
+    let dates = Object.keys(this.data).map((d) => new Date(d));
+    let articles: number[] = Object.values(this.data);
+
+    this.getXScale(dates);
+
+    this.getYScale();
+
+    this.svg = d3
+      .select(this.chartElem.nativeElement)
+      .select('.stacked-area-chart')
+      .append('svg')
+      .attr('height', this.height);
+  }
+
+  getXScale(date: Date[]) {
+    this.xScale = d3
+      .scaleTime()
+      .domain([this.getMinDate(date), this.getMaxDate(date)])
+      .range([this.margin, this.width - 2 * this.margin]);
+  }
+
+  getYScale() {
+    this.yScale = d3.scaleLinear().range([0, this.height - 2 * this.margin]);
+  }
+
+  getMinDate(dates: Date[]) {
+    return dates.reduce(function (a, b) {
+      return a < b ? a : b;
+    });
+  }
+
+  getMaxDate(dates: Date[]) {
+    return dates.reduce(function (a, b) {
+      return a > b ? a : b;
+    });
+  }
+}
